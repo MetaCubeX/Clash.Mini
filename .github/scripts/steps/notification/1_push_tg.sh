ef5227b18b361a0b8e71e317d8d0c023472de63a
JyCyunMe <45283199+jycyunme@users.noreply.github.com>
2021-06-05 17:07:10 +0800

    docs: update RELEASELOG.md
    close #8
    
    Signed-off-by: JyCyunMe <45283199+jycyunme@users.noreply.github.com>

--- .github/scripts/steps/notification/1_push_tg.sh
+++ .github/scripts/steps/notification/1_push_tg.sh
@@ -5,7 +5,7 @@ if [ -f "./RELEASELOG_NOTIFY.md" ]; then
 else
   RELEASE_LOG="./RELEASELOG.md"
 fi
-RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' $RELEASE_LOG)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/Clash-Mini/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"Star\",\"url\":\"https://github.com/Clash-Mini/Clash.Mini\"}]]}")
+RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' $RELEASE_LOG)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"📦下载\",\"url\":\"https://github.com/Clash-Mini/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"⭐点个Star吧\",\"url\":\"https://github.com/Clash-Mini/Clash.Mini\"}]]}")
 IS_OK=$(echo $RLT | jq ".ok")
 if [ $IS_OK ]; then
   MSG_ID=$(echo $RLT | jq ".result.message_id")
--- RELEASELOG.md
+++ RELEASELOG.md
@@ -1,10 +1,33 @@
 ## Release Log
 
-### **v0.1.2.1-pre**
+### `v0.1.3-pre`
 
-Release on *2021-05-30 00:50*
+Release on _**2021-06-05 16:00**_ 👏
+
+**Features**
+
+1. Supported switch proxy on tray menu ([#8](https://github.com/Clash-Mini/Clash.Mini/issues/8))
+2. Optimized some performance
 
 **Fixes**
 
-1. Verification steps for renewing subscriptions
-2. Correction of the text description of the tray menu
+1. Fixed some problems about Tasksch
+2. Set the default value when the Regedit value cannot be obtained
+
+---
+
+## 发行日志
+
+### `v0.1.3-pre`
+
+发布于 _**2021-06-05 16:00**_ 👏
+
+🎉**特性**
+
+1. 支持在托盘菜单切换代理 ([#8](https://github.com/Clash-Mini/Clash.Mini/issues/8))
+2. 优化了一些性能
+
+🎇**修复**
+
+1. 修复任务计划的问题
+2. 无法读取到注册表值时自动设置默认值
