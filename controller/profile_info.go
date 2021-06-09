05c3a0d754758346efb53475749330f14ba94b54
Mazeorz <mazeorz.me@gmail.com>
2021-06-09 13:21:25 +0800

    Fix: Webview Dpi

--- controller/profile_info.go
+++ controller/profile_info.go
@@ -211,7 +211,6 @@ func CheckConfig() (config, controllerPort string) {
 	for scanner.Scan() {
 		if Reg.MatchString(scanner.Text()) {
 			config = Reg.FindStringSubmatch(scanner.Text())[1]
-			fmt.Println("配置名", config)
 			break
 		} else {
 			config = ""
@@ -220,7 +219,6 @@ func CheckConfig() (config, controllerPort string) {
 	for scanner.Scan() {
 		if Reg2.MatchString(scanner.Text()) {
 			controllerPort = Reg2.FindStringSubmatch(scanner.Text())[2]
-			fmt.Println("正则匹配端口", controllerPort)
 			break
 		} else {
 			controllerPort = constant.ControllerPort
--- controller/webview.go
+++ controller/webview.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/Clash-Mini/Clash.Mini/constant"
 	"github.com/Clash-Mini/Clash.Mini/log"
-
 	"github.com/lxn/win"
 	"github.com/skratchdot/open-golang/open"
 	"github.com/zserge/lorca"
@@ -16,21 +15,25 @@ const (
 
 func Dashboard() {
 	_, controllerPort := CheckConfig()
-	fmt.Println("端口号：", controllerPort)
+
+	if dpiScale == 0 {
+		dpiScale = 1
+	}
+
 	xScreen := int(win.GetSystemMetrics(win.SM_CXSCREEN))
 	yScreen := int(win.GetSystemMetrics(win.SM_CYSCREEN))
-	pageWidth := 800
-	pageHeight := 580
+	var pageWidth int32 = 800
+	var pageHeight int32 = 580
+	pageWidth, pageHeight = CalcDpiScaledSize(pageWidth, pageHeight)
 	PageInit := lorca.Bounds{
-		Left:        (xScreen - pageWidth) / 2,
-		Top:         (yScreen - pageHeight) / 2,
-		Width:       pageWidth,
-		Height:      pageHeight,
+		Left:        (xScreen - int(pageWidth)) / 2,
+		Top:         (yScreen - int(pageHeight)) / 2,
+		Width:       int(pageWidth),
+		Height:      int(pageHeight),
 		WindowState: "normal",
 	}
 	localUIUrl := fmt.Sprintf(localUIPattern, constant.Localhost, constant.DashboardPort,
 		constant.Localhost, controllerPort)
-	fmt.Println("控制链接：", localUIUrl)
 	ui, err := lorca.New(localUIUrl, "", 0, 0)
 	if err != nil {
 		log.Errorln("create dashboard failed %v", err)
