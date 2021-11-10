4a8aba56f7352bba1391f544efdc599df9e7b81b
JyCyunMe <45283199+jycyunme@users.noreply.github.com>
2021-11-10 16:23:15 +0800

    chore: ignore uac on GitHub Actions
    Signed-off-by: JyCyunMe <45283199+jycyunme@users.noreply.github.com>

--- util/uac/uac_test.go
+++ util/uac/uac_test.go
@@ -1,8 +1,14 @@
 package uac
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestAmAdmin(t *testing.T) {
+	if value := os.Getenv("GIT_BRANCH"); len(value) > 0 {
+		return
+	}
 	if AmAdmin {
 		t.Fail()
 	}
