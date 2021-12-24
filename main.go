bbda4f6e341eea8444836f2dc59af86a2c705790
Maze.tsz <mazeorz.me@gmail.com>
2021-12-25 00:36:59 +0800

    [Fix] UserInfo

--- main.go
+++ main.go
@@ -9,19 +9,21 @@ import (
 	cConfig "github.com/Clash-Mini/Clash.Mini/config"
 	"github.com/Clash-Mini/Clash.Mini/constant"
 	cI18n "github.com/Clash-Mini/Clash.Mini/constant/i18n"
+	"github.com/Clash-Mini/Clash.Mini/controller"
 	"github.com/Clash-Mini/Clash.Mini/mixin"
 	"github.com/Clash-Mini/Clash.Mini/util/common"
 	"github.com/Clash-Mini/Clash.Mini/util/uac"
+	C "github.com/Dreamacro/clash/constant"
 	"github.com/JyCyunMe/go-i18n/i18n"
 	"github.com/lxn/walk"
 	"github.com/lxn/win"
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
 
 	_ "github.com/Clash-Mini/Clash.Mini/app/bridge/start"
 	"github.com/Clash-Mini/Clash.Mini/log"
-
 	"github.com/Dreamacro/clash/config"
 )
 
@@ -29,13 +31,15 @@ func main() {
 
 	Restart()
 
+	Name := cConfig.GetProfile()
+	exist, configName := controller.CheckConfig(Name)
+	if exist {
+		configFile := path.Join(constant.ProfileDir, configName)
+		C.SetConfig(configFile)
+	}
 	// init mmdb and geosite
 	if err := config.Init(common.GetExecutablePath()); err != nil {
-		log.Fatalln("Initial configuration directory error: %s", err.Error())
-	}
-
-	if err := os.Remove(common.GetExecutablePath("config.yaml")); err != nil {
-
+		log.Errorln("Initial configuration directory error: %s", err.Error())
 	}
 
 	sigCh := make(chan os.Signal, 1)
