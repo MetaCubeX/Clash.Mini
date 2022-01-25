af42431a0104afdea71b779012fb9cfb53dc91c1
MetaCubeX <maze.y2b@gmail.com>
2022-01-25 16:46:18 +0800

    [Fix]Version

--- static/gh-pages/assets/Select.be3bf7e5.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./vendor.2c7996a3.js";const l="_select_bswl3_1";var o={select:l};function m({options:s,selected:c,onChange:r}){return t.exports.createElement("select",{className:o.select,value:c,onChange:r},s.map(([e,a])=>t.exports.createElement("option",{key:e,value:e},a)))}export{m as S};
